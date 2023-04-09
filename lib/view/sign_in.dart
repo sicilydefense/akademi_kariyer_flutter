032cf1c787d0a29bddf7622aadfb5ca88796a4d9
Ezgi Gümüştekin <107347424+sicilydefense@users.noreply.github.com>
2023-04-09 21:48:55 +0300

    Add files via upload

--- lib/view/sign_in.dart
+++ lib/view/sign_in.dart
@@ -127,11 +127,13 @@ class _SignInScreenState extends State<SignInScreen> {
                           "Giriş Yap",
                           style: TextStyle(color: academyBlack),
                         )),
+                    /*
                     SizedBox(height: 15.0),
                     ElevatedButton(
                       onPressed: () {
                         // TODO: Handle login
                       },
+                      
                       child: Text('Google ile Devam Et'),
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Color.fromARGB(210, 66, 131, 241),
@@ -143,6 +145,7 @@ class _SignInScreenState extends State<SignInScreen> {
                         ),
                       ),
                     ),
+                    */
                     TextButton(
                         onPressed: () {
                           Navigator.of(context).pushReplacement(
